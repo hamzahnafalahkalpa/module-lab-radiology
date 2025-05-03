016119470b49e1e4d9ab4910a5827eb2c5b6a223
Hamzah <hamzahnafalah@gmail.com>
2025-05-03 12:43:19 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -7,24 +7,26 @@ use Hanafalah\ModuleLabRadiology\{
 };
 
 return [
-    'contracts' => [
-        'laboratirium'                 => Contracts\Laboratorium::class,
-        'lab_visit_registration'       => Contracts\LabVisitRegistration::class,
-        'module_lab_radiology'         => Contracts\ModuleLabRadiology::class,
-        'radiology'                    => Contracts\Radiology::class,
-        'radiology_visit_registration' => Contracts\RadiologyVisitRegistration::class,
-        'sampling'                     => Contracts\Sampling::class,
-        'sampling_laboratory'          => Contracts\SamplingLaboratory::class
+    'namespace' => 'Hanafalah\\ModuleLabRadiology',
+    'app' => [
+        'contracts' => [
+            //ADD YOUR CONTRACTS HERE
+        ]
+    ],
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts',
+        'schema' => 'Schemas',
+        'database' => 'Database',
+        'data' => 'Data',
+        'resource' => 'Resources',
+        'migration' => '../assets/database/migrations'
     ],
     'commands' => [
         ModuleLabRadiologyCommands\InstallMakeCommand::class
     ],
     'database' => [
         'models' => [
-            'Laboratorium' => ModuleLabRadiologyModels\LabRadiology\Laboratorium\Laboratorium::class,
-            'Radiology'    => ModuleLabRadiologyModels\LabRadiology\Radiology\Radiology::class,
-            'Sampling'     => ModuleLabRadiologyModels\LabRadiology\Laboratorium\Sampling\Sampling::class,
-            'SamplingLaboratory' => ModuleLabRadiologyModels\LabRadiology\Laboratorium\Sampling\SamplingLaboratory::class
         ]
     ]
 ];
