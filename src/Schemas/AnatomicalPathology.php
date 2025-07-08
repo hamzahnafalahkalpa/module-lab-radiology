84bf30fb514ee9b26e565894b1393bd6770fd5db
Hamzah <hamzahnafalah@gmail.com>
2025-07-08 20:16:00 +0700

    update modules

--- src/Schemas/AnatomicalPathology.php
+++ src/Schemas/AnatomicalPathology.php
@@ -10,20 +10,20 @@ use Illuminate\Database\Eloquent\Builder;
 class AnatomicalPathology extends LabRadiology implements ContractsAnatomicalPathology
 {
     protected string $__entity = 'AnatomicalPathology';
-    public static $clinical_pathology_model;
+    public static $anatomical_pathology_model;
     //protected mixed $__order_by_created_at = false; //asc, desc, false
 
     protected array $__cache = [
         'index' => [
-            'name'     => 'clinical_pathology',
-            'tags'     => ['clinical_pathology', 'clinical_pathology-index'],
+            'name'     => 'anatomical_pathology',
+            'tags'     => ['anatomical_pathology', 'anatomical_pathology-index'],
             'duration' => 24 * 60
         ]
     ];
 
     public function prepareStoreAnatomicalPathology(AnatomicalPathologyData $anatomical_pathology_dto): Model{
-        $clinical_pathology = parent::prepareStoreLabRadiology($anatomical_pathology_dto);
-        return static::$clinical_pathology_model = $clinical_pathology;
+        $anatomical_pathology = parent::prepareStoreLabRadiology($anatomical_pathology_dto);
+        return static::$anatomical_pathology_model = $anatomical_pathology;
     }
 
     public function anatomicalPathology(mixed $conditionals = null): Builder{
